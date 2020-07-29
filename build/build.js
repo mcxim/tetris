a3d046040c54161cde2b3829fb7d3d19539fd6f2
Maxim Gelfand <gelfandmaxim@gmail.com>
2020-07-30 02:50:24 +0300

    9 B'av high on low sleep hackathon yay.
    Co-authored-by: ErelZ03 <erelzack@gmail.com>
    Co-authored-by: Kpheer <kfirabrahamhadar@gmail.com>

--- build/build.js
+++ build/build.js
@@ -66,7 +66,18 @@ function generatePolygon() {
 var cellPx = 20;
 var horizontalCells = 10;
 var verticalCells = 20;
+var timeStep = 1000;
+var possibleColors = [
+    "blue",
+    "red",
+    "green",
+    "yellow",
+    "purple",
+    "orange",
+];
 var cells = [];
+var steadyStateCells = [];
+var lastFrame = [];
 var posCells = [];
 var pos = [0, 0];
 var StickerState = (function () {
@@ -84,15 +95,7 @@ var emptyColor;
 function setup() {
     createCanvas(cellPx * horizontalCells, cellPx * verticalCells);
     emptyColor = color(emptyColorString);
-    var possibleColors = [
-        "blue",
-        "red",
-        "green",
-        "yellow",
-        "purple",
-        "orange",
-    ];
-    initNewShape(possibleColors);
+    console.log(steadyStateCells);
     strokeWeight(4);
     stroke(51);
     fill(emptyColorString);
@@ -104,6 +107,10 @@ function setup() {
             posCells[i][j] = [j * cellPx, i * cellPx];
         }
     }
+    setInterval(timeIt, timeStep);
+    console.log(cells);
+    initNewShape(possibleColors);
+    console.log(steadyStateCells);
     console.log(applySticker());
 }
 function draw() {
@@ -119,18 +126,20 @@ function drawGrid() {
     }
     pop();
 }
-function rotate(matrix) {
+function rotateMatrix(matrix) {
     return matrix[0].map(function (_, colIndex) {
         return matrix.map(function (row) { return row[colIndex]; }).reverse();
     });
 }
 function applySticker() {
+    cells = JSON.parse(JSON.stringify(steadyStateCells));
     var size = stickerState.size;
     var matrix = stickerState.matrix;
     var _a = stickerState.coords, y = _a[0], x = _a[1];
     var stickerColor = stickerState.colorString;
     for (var ySticker = 0; ySticker < size; ySticker++) {
         for (var xSticker = 0; xSticker < size; xSticker++) {
+            console.log(ySticker, xSticker, JSON.stringify(matrix));
             if (!!matrix[ySticker][xSticker]) {
                 if (cells[y + ySticker][x + xSticker] ===
                     emptyColorString &&
@@ -138,9 +147,12 @@ function applySticker() {
                     x + xSticker <= horizontalCells - 1 &&
                     0 <= y + ySticker &&
                     y + ySticker <= verticalCells - 1) {
+                    lastFrame = JSON.parse(JSON.stringify(cells));
                     cells[y + ySticker][x + xSticker] = stickerColor;
                 }
                 else {
+                    console.log("Du und ich sind falsch");
+                    cells = JSON.parse(JSON.stringify(lastFrame));
                     return false;
                 }
             }
@@ -149,32 +161,68 @@ function applySticker() {
     return true;
 }
 function initNewShape(colors) {
+    steadyStateCells = JSON.parse(JSON.stringify(cells));
     var initCoords = [0, 0];
     var _a = generatePolygon(), shape = _a[0], size = _a[1];
-    var leftMost = 0;
-    for (var i = size; i > 0; i--) {
-        for (var j = 0; j < size; j++) {
-            if (shape[j][i] == 1) {
-                leftMost = i;
-                break;
-            }
-        }
-        break;
+    console.log("before: ", shape);
+    var rotations = floor(random(0, 4));
+    for (var i = 0; i < rotations; i++) {
+        shape = rotateMatrix(shape);
     }
-    var delta = floor(random() * (horizontalCells - size + leftMost));
+    console.log("after: ", shape);
+    var leftMost = 0;
+    leftMost = getLeftMost(shape, size);
+    var delta = floor(random(0, horizontalCells - leftMost - 1 + 1));
     initCoords = [0, 0 + delta];
     stickerState = new StickerState(shape, size, initCoords, colors[floor(random(0, colors.length))]);
 }
 function keyPressed() {
     if (keyCode === UP_ARROW) {
-        stickerState.matrix = rotate(stickerState.matrix);
+        stickerState.matrix = rotateMatrix(stickerState.matrix);
     }
     else if (keyCode === DOWN_ARROW) {
+        stickerState.coords = [
+            stickerState.coords[0] + 1,
+            stickerState.coords[1],
+        ];
+    }
+    else if (keyCode === RIGHT_ARROW) {
         stickerState.coords = [
             stickerState.coords[0],
             stickerState.coords[1] + 1,
         ];
     }
+    else if (keyCode === LEFT_ARROW) {
+        stickerState.coords = [
+            stickerState.coords[0],
+            stickerState.coords[1] - 1,
+        ];
+    }
     applySticker();
+}
+function timeIt() {
+    stickerState.coords = [
+        stickerState.coords[0] + 1,
+        stickerState.coords[1],
+    ];
+    if (!applySticker()) {
+        stickerState.coords = [
+            stickerState.coords[0] - 1,
+            stickerState.coords[1],
+        ];
+        initNewShape(possibleColors);
+    }
+    console.log("tick");
+}
+function getLeftMost(shape, size) {
+    var leftMost = 0;
+    for (var i = size - 1; i >= 0; i--) {
+        for (var j = 0; j < size; j++) {
+            if (shape[j][i] == 1) {
+                leftMost = i;
+                return leftMost;
+            }
+        }
+    }
 }
 //# sourceMappingURL=build.js.map
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -2,6 +2,7 @@ const cellPx = 20;
 
 const horizontalCells = 10;
 const verticalCells = 20;
+const timeStep = 1000;
 
 type Coords = [number, number];
 
@@ -15,8 +16,18 @@ type ColorString =
   | "white"
   | "pink";
 type ColorMatrix = ColorString[][];
+const possibleColors: ColorString[] = [
+  "blue",
+  "red",
+  "green",
+  "yellow",
+  "purple",
+  "orange",
+];
 
 let cells: ColorMatrix = [];
+let steadyStateCells: ColorMatrix = [];
+let lastFrame: ColorMatrix = [];
 let posCells: Coords[][] = [];
 let pos: Coords = [0, 0];
 
@@ -31,7 +42,7 @@ class StickerState {
 
 let stickerState: StickerState;
 
-const emptyColorString = "white";
+const emptyColorString: ColorString = "white";
 let emptyColor: p5.Color;
 
 function setup() {
@@ -40,15 +51,9 @@ function setup() {
     cellPx * verticalCells
   );
   emptyColor = color(emptyColorString);
-  const possibleColors: ColorString[] = [
-    "blue",
-    "red",
-    "green",
-    "yellow",
-    "purple",
-    "orange",
-  ];
-  initNewShape(possibleColors);
+  
+
+  console.log(steadyStateCells);
   strokeWeight(4);
   stroke(51);
   fill(emptyColorString);
@@ -60,6 +65,11 @@ function setup() {
       posCells[i][j] = [j * cellPx, i * cellPx];
     }
   }
+
+  setInterval(timeIt, timeStep);
+  console.log(cells);
+  initNewShape(possibleColors);
+  console.log(steadyStateCells);
   console.log(applySticker());
 }
 
@@ -78,19 +88,25 @@ function drawGrid() {
   pop();
 }
 
-function rotate(matrix: any[][]) {
+function rotateMatrix(matrix: Booly[][]): Booly[][] {
   return matrix[0].map((_, colIndex) =>
     matrix.map((row) => row[colIndex]).reverse()
   );
 }
 
 function applySticker(): Boolean {
+  cells = JSON.parse(JSON.stringify(steadyStateCells));
   let size = stickerState.size;
   let matrix = stickerState.matrix;
   let [y, x] = stickerState.coords;
   let stickerColor = stickerState.colorString;
   for (let ySticker = 0; ySticker < size; ySticker++) {
     for (let xSticker = 0; xSticker < size; xSticker++) {
+      console.log(
+        ySticker,
+        xSticker,
+        JSON.stringify(matrix)
+      );
       if (!!matrix[ySticker][xSticker]) {
         if (
           cells[y + ySticker][x + xSticker] ===
@@ -100,8 +116,11 @@ function applySticker(): Boolean {
           0 <= y + ySticker &&
           y + ySticker <= verticalCells - 1
         ) {
+          lastFrame = JSON.parse(JSON.stringify(cells));
           cells[y + ySticker][x + xSticker] = stickerColor;
         } else {
+          console.log("Du und ich sind falsch");
+          cells = JSON.parse(JSON.stringify(lastFrame));
           return false;
         }
       }
@@ -111,28 +130,24 @@ function applySticker(): Boolean {
 }
 
 function initNewShape(colors: ColorString[]) {
+  steadyStateCells = JSON.parse(JSON.stringify(cells));
   let initCoords: Coords = [0, 0];
   let [shape, size] = generatePolygon();
 
-  let leftMost = 0;
-  //Check leftShape
-  for (let i = size; i > 0; i--) {
-    //Column--------------[?][i]
-    //Check what is the leftmost filled cell of the shape
-    //Checks column whise
-    for (let j = 0; j < size; j++) {
-      //Row-------------[j][i]
-      if (shape[j][i] == 1) {
-        leftMost = i;
-        break;
-      }
-    }
-    break;
+  console.log("before: ", shape);
+  let rotations = floor(random(0, 4));
+  for (let i = 0; i < rotations; i++) {
+    shape = rotateMatrix(shape);
   }
+  console.log("after: ", shape);
 
+  let leftMost = 0;
+  leftMost = getLeftMost(shape, size);
   let delta = floor(
-    random() * (horizontalCells - size + leftMost)
+    random(0, horizontalCells - leftMost - 1 + 1)
   );
+  //console.log(leftMost);
+  //let delta = horizontalCells - leftMost - 1;
   initCoords = [0, 0 + delta]; // y, x
 
   stickerState = new StickerState(
@@ -145,12 +160,52 @@ function initNewShape(colors: ColorString[]) {
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-    stickerState.matrix = rotate(stickerState.matrix);
+    stickerState.matrix = rotateMatrix(stickerState.matrix);
   } else if (keyCode === DOWN_ARROW) {
+    stickerState.coords = [
+      stickerState.coords[0] + 1,
+      stickerState.coords[1],
+    ];
+  } else if (keyCode === RIGHT_ARROW) {
     stickerState.coords = [
       stickerState.coords[0],
       stickerState.coords[1] + 1,
     ];
+  } else if (keyCode === LEFT_ARROW) {
+    stickerState.coords = [
+      stickerState.coords[0],
+      stickerState.coords[1] - 1,
+    ];
   }
   applySticker();
+}
+
+function timeIt() {
+  // 1 time step = 1000 millisecond
+  stickerState.coords = [
+    stickerState.coords[0] + 1,
+    stickerState.coords[1],
+  ];
+  if(!applySticker()){
+    stickerState.coords = [
+      stickerState.coords[0] - 1,
+      stickerState.coords[1],
+    ];
+    initNewShape(possibleColors);
+  }
+  console.log("tick");
+}
+
+function getLeftMost(shape: Booly[][], size: number) {
+  let leftMost = 0;
+  for (let i = size - 1; i >= 0; i--) {
+    //-----[?][i]
+    for (let j = 0; j < size; j++) {
+      //-----[j][i]
+      if (shape[j][i] == 1) {
+        leftMost = i;
+        return leftMost;
+      }
+    }
+  }
 }
